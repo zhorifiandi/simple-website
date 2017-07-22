1f8b1d26f2161f4d0938a370716fa30cd5ff066b
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2017-07-22 21:05:07 +0700

    login logout signup

---
+++ simple-website/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  resources :users
+end
